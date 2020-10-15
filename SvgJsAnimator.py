9141ab1c42b74ab36c34092cc0eedc015c697418
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-15 07:52:37 -0400

    JsAnimator: add group declaration utilities

--- SvgJsAnimator.py
+++ SvgJsAnimator.py
@@ -40,3 +40,41 @@ class SvgJsPath:
         print(
             f'let {self.js_name} = {{path: {query_js}, length: {query_js}.getTotalLength()}}',
             file=self.out)
+
+
+class SvgJsGroup:
+    """Maps an Svg Group to a JavaScript variable.
+
+    This class manipulates aspects of an Svg group through a JS format.
+
+    It assumes the parent Svg container has been included in an HTML document.
+    """
+
+    gid = -1
+
+    def gen_unique_name():
+        SvgJsGroup.gid += 1
+        return 'group' + str(SvgJsGroup.gid)
+
+    def __init__(self, node: ET.Element, out):
+        """ Declares `node` as a JS variable inside `out`.
+
+        This causes an instantiation of SvgJsPath for each Svg path that is a
+        direct child of `node`. The group JS variable will be an array of all
+        such children.
+        `node` must have the Svg xml group tag.
+        The resulting JS variable will have name `self.js_name`.
+        `out` must be anything accepted by the `file` argument of standard
+        `print`.
+        """
+        assert node.tag == SvgUtils._group_tag, "SvgJsGroup must be created with an Svg group"
+        self.print = lambda msg: print(msg, file=out)
+        self.node = node
+        self.js_name = SvgJsGroup.gen_unique_name()
+        self.paths = [SvgJsPath(path, out)
+                      for path in SvgUtils.svg_paths(node)]
+        self.__declare()
+
+    def __declare(self):
+        list_str = ', '.join([f'{path.js_name}' for path in self.paths])
+        self.print(f'let {self.js_name} = [{list_str}]')
