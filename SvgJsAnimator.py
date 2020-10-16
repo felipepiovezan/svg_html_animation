75c091a90733cf2cded2991d0c0a7073b50c2111
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-16 07:39:14 -0400

    JsAnimator: introduce SvgJsAnimator class
    This class declares functions for manipulating the next frame in
    an animation and for clearing the "canvas". It also maintains an
    internal queue o animation elements.

--- SvgJsAnimator.py
+++ SvgJsAnimator.py
@@ -81,3 +81,67 @@ class SvgJsGroup:
     def __declare(self):
         list_str = ', '.join([f'{path.js_name}' for path in self.paths])
         self.print(f'let {self.js_name} = [{list_str}]')
+
+
+class SvgJsAnimator:
+    """Manipulates HTML properties of SvgPaths using JS.
+
+    Through the manipulation of `style.strokeDasharray` and
+    `style.strokeDashoffset`, this class is able to hide or display [pieces of]
+    paths. By interacting with the `window.requestAnimationFrame` interface, it
+    creates the effect that paths are being drawn.
+    """
+
+    def __init__(self, out):
+        """Prints the boilerplate JS code to `out`."""
+
+        self.print = lambda msg: print(msg, file=out)
+        self.js_animation_queue = 'animation_queue'
+        self.js_drawing_idx = 'drawing_idx'
+        self.print(f'let {self.js_animation_queue} = []')
+        self.print(f'let {self.js_drawing_idx} = 0')
+        self.length_per_ms = .5
+
+        self.js_clear_path_foo = 'clear_path'
+        self._print_clear_path_foo()
+
+        self.js_next_frame_foo = 'next_frame'
+        self._print_next_frame_foo()
+
+    def _print_clear_path_foo(self):
+        js_path_arg = 'path'
+        js_length = f'{js_path_arg}.length'
+        self.print(f'''
+        function {self.js_clear_path_foo}({js_path_arg}) {{
+            {js_path_arg}.path.style.strokeDasharray = {js_length} + " " + {js_length}
+            {js_path_arg}.path.style.strokeDashoffset = {js_length}
+        }}
+        ''')
+
+    def _print_next_frame_foo(self):
+        js_current_path = f'{self.js_animation_queue}[{self.js_drawing_idx}]'
+        self.print(f'''
+    let start;
+    let handle = 0;
+    function {self.js_next_frame_foo}(timestamp) {{
+      if ({self.js_drawing_idx} == {self.js_animation_queue}.length) {{
+        window.cancelAnimationFrame(handle);
+        return
+      }}
+
+      if (start === undefined)
+        start = timestamp;
+
+      const elapsed = timestamp - start;
+      const total_time =  {js_current_path}.length / {self.length_per_ms};
+      const percentage = elapsed/total_time
+      const progress = Math.min(1, percentage)
+      {js_current_path}.path.style.strokeDashoffset = Math.floor({js_current_path}.length * (1 - progress));
+      handle = window.requestAnimationFrame(step);
+
+      if (progress === 1) {{
+        start = timestamp;
+        {self.js_drawing_idx}++;
+      }}
+    }}
+    ''')
