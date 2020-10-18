22ebaddd780889dd6b9e4df65f8a6ac6319d2a43
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-18 10:07:43 -0400

    example: add end-to-end example
    This should serve as a model for using this library.

---
+++ example.py
@@ -0,0 +1,20 @@
+from HtmlUtils import HtmlPrinter
+from SvgUtils import svg_groups
+from SvgJsAnimator import SvgJsAnimator, SvgJsGroup
+import xml.etree.ElementTree as ET
+
+html = HtmlPrinter("temp.html")
+with html.html_ctx():
+    with open("abstract_machine.svg", "r") as svg_file:
+        html.print(svg_file.read())
+
+with html.js_ctx():
+    animator = SvgJsAnimator(html.file)
+    root = ET.parse('abstract_machine.svg').getroot()
+    animator.set_dimensions_to_100pc(root)
+    groups_to_draw = [SvgJsGroup(group, html.file)
+                      for group in svg_groups(root)]
+    [(animator.add_group_to_queue(group), animator.add_stop_event_to_queue())
+     for group in groups_to_draw]
+    animator.clear_paths_from_screen()
+    animator.start_animation()
