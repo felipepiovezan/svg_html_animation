356f1e002250a971ae87b76871c1e5f504307c73
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-24 19:58:35 -0400

    documentation: fix docs for Path/Camera events

--- PathEvent.py
+++ PathEvent.py
@@ -3,7 +3,19 @@ import xml.etree.ElementTree as ET
 
 
 class PathEvent:
+    """Class to create a path animation.
+
+    Manipulates the style.strokeDasharray and style.strokeDashoffset properties
+    of paths in order to create the illusion of a path drawing itself.
+    """
+
     def __init__(self, node: ET.Element, out):
+        """Creates a PathEvent from an SVG Path node.
+
+        Note that process_event takes as an argument the number of milliseconds
+        elapsed since this event began.
+        """
+
         assert SvgUtils.is_path(node)
 
         html_id = SvgUtils.get_id(node)
