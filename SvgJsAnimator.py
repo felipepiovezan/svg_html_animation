de3427d072ef912ace5d7c144749d335b64a2e3e
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-25 07:01:16 -0400

    Animator: add SvgJsAnimator class

---
+++ SvgJsAnimator.py
@@ -0,0 +1,121 @@
+import xml.etree.ElementTree as ET
+import SvgUtils
+from CameraEvent import CameraEvent
+from SequentialEventContainer import SequentialEventContainer
+from ParallelEventContainer import ParallelEventContainer
+from PathEvent import PathEvent
+
+
+class SvgJsAnimator:
+    """Creates animations by manipulating HTML properties of SvgPaths using JS.
+
+    Through the manipulation of `style.strokeDasharray` and
+    `style.strokeDashoffset`, this class is able to hide or display [pieces of]
+    paths. By interacting with the `window.requestAnimationFrame` interface, it
+    creates the effect that paths are being drawn gradually.
+
+    No more than one such object should be created for the same file, as it
+    relies on global state for some of its functions.
+    """
+
+    def __init__(self, out, events, svg_root: ET.Element):
+        """Prints the boilerplate JS code to `out`.
+
+        Initializes the camera to be the bounding box of the entire svg.
+        Sets the svg height and width to 100%, effectively fitting the entire
+        file to the space allocated for the svg container.
+
+        The animation event queue is set to `events`, which must be an array
+        of [Path|Camera]Events or [Parallel|Sequential]Containers.
+
+        A pause is inserted between two consecutive elements of `events`, and
+        the animation is resumed upon certain keyboard events.
+        """
+        # Helper function to print to `out`.
+        self.out = out
+        self.print = lambda msg: print(msg, file=out)
+
+        # Save root and give it a name.
+        assert SvgUtils.is_root(svg_root)
+        self.svg_root = svg_root
+        root_id = SvgUtils.get_id(svg_root)
+        self.js_svg_root = 'svg_root'
+        self.print(
+            f'{self.js_svg_root} = document.getElementById("{root_id}")')
+
+        # Reset root dimensions to 100% so that BBox setting works properly.
+        self.set_dimensions_to_100pc()
+
+        # Print all JS classes
+        ParallelEventContainer.print_js_class(self.out)
+        SequentialEventContainer.print_js_class(self.out)
+        PathEvent.print_js_class(self.out)
+        CameraEvent.print_js_class(self.out)
+
+        self.js_event_idx = 'event_idx'
+        self.js_event_list = 'event_list'
+        events_str = ', '.join([event.js_name for event in events])
+        self.print(f'let {self.js_event_list} = [{events_str}]')
+
+        self.js_listen_to_kb = 'listen_to_kb'
+        self.print(f'let {self.js_listen_to_kb} = false')
+
+        self.js_foo_next_frame = 'next_frame'
+        self._print_next_frame_foo()
+
+        self._print_keyboard_event_foo()
+
+    def _print_next_frame_foo(self):
+        self.print(f'''
+          let handle = 0;
+          function {self.js_foo_next_frame}(timestamp) {{
+            if ({self.js_event_idx} == {self.js_event_list}.length) {{
+              window.cancelAnimationFrame(handle);
+              return
+            }}
+
+            let finished = {self.js_event_list}[{self.js_event_idx}].process_event(timestamp);
+            if (finished) {{
+              window.cancelAnimationFrame(handle);
+              {self.js_listen_to_kb} = true;
+              {self.js_event_idx}++;
+            }}
+          }}''')
+
+    def _print_keyboard_event_foo(self):
+        self.print(f'''
+          document.addEventListener('keydown', (event) => {{
+            if ({self.js_listen_to_kb} == false)
+              return;
+            switch (event.key) {{
+              case "Down": // IE/Edge
+              case "ArrowDown":
+              case "Right": // IE/Edge
+              case "ArrowRight":
+              case "Enter":
+              case " ":
+                {self.js_listen_to_kb} = false
+                handle = window.requestAnimationFrame({self.js_foo_next_frame});
+              default:
+                return;
+            }}
+          }}, false);''')
+
+    def set_dimensions_to_100pc(self):
+        """Remove the `width` and `height` attributes from the SVG node.
+
+        If these attributes are empty, they instead become 100%, and the svg
+        container will take all the space available on the top level container,
+        scaling accordingly.
+
+        This allows further manipulation of the viewbox, setting its dimensions
+        to be those of a rectangle, effectively manipulating the "camera".
+        """
+
+        self.print(f'{self.js_svg_root}.setAttribute("width" , "100%")')
+        self.print(f'{self.js_svg_root}.setAttribute("height", "100%")')
+
+    def start_animation(self):
+        """Output JS call to function that starts the animation."""
+
+        self.print(f'window.requestAnimationFrame({self.js_foo_next_frame})')
---
+++ test_SvgJsAnimator.py
@@ -0,0 +1,53 @@
+from SvgVisitors import SimpleVisitor
+from SvgJsAnimator import SvgJsAnimator
+import io
+import unittest
+import xml.etree.ElementTree as ET
+
+
+class TestSvgJsAnimator(unittest.TestCase):
+    nested_root = ET.fromstring('''
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         xmlns:svg="http://www.w3.org/2000/svg"
+         id="root_id">
+         <rect
+            id="cam0"
+            x="10"
+            y="20"
+            width="30"
+            height="40"/>
+         <g id="seq_group">
+            <path id="path0"/>
+            <g id="par_group">
+              <path id="path1"/>
+              <path id="path2"/>
+              <path id="path3"/>
+            </g>
+         </g>
+      </svg>''')
+
+    def test_grouped_root(self):
+        out = io.StringIO()
+        visitor = SimpleVisitor(out)
+        events = visitor.visit_root(TestSvgJsAnimator.nested_root)
+        animator = SvgJsAnimator(out, events, TestSvgJsAnimator.nested_root)
+
+        out = out.getvalue()
+
+        cam_event = events[0]
+        seq_container = events[1]
+        self.assertIn(
+            f'let {animator.js_event_list} = [{cam_event.js_name}, {seq_container.js_name}]',
+            out)
+
+        self.assertIn(f'function {animator.js_foo_next_frame}', out)
+        self.assertIn(
+            f'{animator.js_svg_root}.setAttribute("width" , "100%")', out)
+        self.assertIn(
+            f'{animator.js_svg_root}.setAttribute("height", "100%")', out)
+        self.assertIn(f"document.addEventListener('keydown', (event)", out)
+
+
+if __name__ == '__main__':
+    unittest.main()
