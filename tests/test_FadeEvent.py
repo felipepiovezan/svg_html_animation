6480547663402eb4d490cf2a34b65efbacf2f6e3
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2022-01-02 11:08:34 -0300

    animator: rename CircleEvent to FadeEvent

--- tests/test_FadeEvent.py
+++ tests/test_FadeEvent.py
@@ -1,11 +1,11 @@
 from svganimator import SvgUtils
-from svganimator.CircleEvent import CircleEvent
+from svganimator.FadeEvent import FadeEvent
 import io
 import unittest
 import xml.etree.ElementTree as ET
 
 
-class TestCircleEvent(unittest.TestCase):
+class TestFadeEvent(unittest.TestCase):
     root = ET.fromstring('''
       <svg
          xmlns="http://www.w3.org/2000/svg"
@@ -18,19 +18,19 @@ class TestCircleEvent(unittest.TestCase):
 
     def test_ctor(self):
         buff = io.StringIO()
-        event = CircleEvent(TestCircleEvent.circle, buff)
+        event = FadeEvent(TestFadeEvent.circle, buff)
         self.assertIsNotNone(event.js_name)
 
         out = buff.getvalue()
-        self.assertRegex(out, f'let {event.js_name} = new CircleEvent')
+        self.assertRegex(out, f'let {event.js_name} = new FadeEvent')
         self.assertRegex(out, f'{event.js_name}.clear_from_screen()')
 
     def test_print_class(self):
         buff = io.StringIO()
-        CircleEvent.print_js_class(buff)
+        FadeEvent.print_js_class(buff)
         out = buff.getvalue()
-        self.assertIn('class CircleEvent', out)
-        self.assertIn('constructor(circle)', out)
+        self.assertIn('class FadeEvent', out)
+        self.assertIn('constructor(obj, total_time = 0.1)', out)
         self.assertIn('process_event(elapsed, finish_requested)', out)
         self.assertIn('clear_from_screen()', out)
         self.assertIn('undo()', out)
